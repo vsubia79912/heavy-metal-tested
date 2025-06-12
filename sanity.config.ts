b98b968a7d2da5a28a616557225d5349db199fad
vsubia79912 <victor@insiderclicks.com>
2025-06-11 22:36:50 -0500

    Fix: Use correct import path for presentationTool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {presentationTool} from '@sanity/presentation'
+import {presentationTool} from 'sanity/presentation'
 import {schema} from './sanity/schema'
 
 export default defineConfig({
